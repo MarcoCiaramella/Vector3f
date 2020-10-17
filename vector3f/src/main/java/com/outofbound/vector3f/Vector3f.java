a3adfe13392151af74c02c7fe5e86ec59bdabe26
MarcoCiaramella <marcociaramella87@gmail.com>
2020-10-17 17:28:13 +0200

    new version

--- vector3f/src/main/java/com/outofbound/vector3f/Vector3f.java
+++ vector3f/src/main/java/com/outofbound/vector3f/Vector3f.java
@@ -32,52 +32,58 @@ public class Vector3f implements Serializable {
         this(v.x,v.y,v.z);
     }
 
-    public void multS(Vector3f res, float s){
+    public Vector3f multS(float s, Vector3f res){
         res.x = x*s;
         res.y = y*s;
         res.z = z*s;
+        return res;
     }
 
-    public void multS(float s){
+    public Vector3f multS(float s){
         x *= s;
         y *= s;
         z *= s;
+        return this;
     }
 
-    public void cross(Vector3f res, Vector3f v){
+    public Vector3f cross(Vector3f v, Vector3f res){
         float crossX = y * v.z - z * v.y;
         float crossY = z * v.x - x * v.z;
         float crossZ = x * v.y - y * v.x;
         res.x = crossX;
         res.y = crossY;
         res.z = crossZ;
+        return res;
     }
 
-    public void cross(Vector3f v){
+    public Vector3f cross(Vector3f v){
         float crossX = y * v.z - z * v.y;
         float crossY = z * v.x - x * v.z;
         float crossZ = x * v.y - y * v.x;
         x = crossX;
         y = crossY;
         z = crossZ;
+        return this;
     }
 
     public float dot(Vector3f v){
         return x * v.x + y * v.y + z * v.z;
     }
 
-    public void normalize(){
+    public Vector3f normalize(){
         float length = length();
         x /= length;
         y /= length;
         z /= length;
+        return this;
     }
 
-    public void normalize(Vector3f res){
+    public Vector3f normalize(Vector3f res){
         float length = length();
         res.x = x/length;
         res.y = y/length;
         res.z = z/length;
+        return res;
     }
 
     private void to4f(){
@@ -93,10 +99,11 @@ public class Vector3f implements Serializable {
         z = vector4f[2];
     }
 
-    public void multiplyMV(float[] m, Vector3f res){
+    public Vector3f multiplyMV(float[] m, Vector3f res){
         to4f();
         Matrix.multiplyMV(res.vector4f,0, m,0,vector4f,0);
         res.to3f();
+        return res;
     }
 
     public float[] multiplyMV4f(float[] m, Vector3f res){
@@ -106,78 +113,66 @@ public class Vector3f implements Serializable {
         return res.vector4f;
     }
 
-    public void rotate(float a, float x, float y, float z){
+    public Vector3f rotate(float a, float x, float y, float z){
         Matrix.setIdentityM(m,0);
         Matrix.rotateM(m,0,a,x,y,z);
         multiplyMV(m,this);
+        return this;
     }
 
-    public void rotate(Vector3f res, float a, float x, float y, float z){
+    public Vector3f rotate(float a, float x, float y, float z, Vector3f res){
         Matrix.setIdentityM(m,0);
         Matrix.rotateM(m,0,a,x,y,z);
         res.copy(this);
         multiplyMV(m,res);
+        return res;
     }
 
-    public void translate(Vector3f shift){
+    public Vector3f translate(Vector3f shift){
         Matrix.setIdentityM(m,0);
         Matrix.translateM(m,0,shift.x,shift.y,shift.z);
         multiplyMV(m,this);
+        return this;
     }
 
-    public void translate(Vector3f res, Vector3f shift){
+    public Vector3f translate(Vector3f shift, Vector3f res){
         Matrix.setIdentityM(m,0);
         Matrix.translateM(m,0,shift.x,shift.y,shift.z);
         res.copy(this);
         multiplyMV(m,res);
+        return res;
     }
 
     public float length(){
         return (float)Math.sqrt(x*x + y*y + z*z);
     }
 
-    public void add(Vector3f res, Vector3f v){
+    public Vector3f add(Vector3f v, Vector3f res){
         res.x = x + v.x;
         res.y = y + v.y;
         res.z = z + v.z;
+        return res;
     }
 
-    public void add(Vector3f v){
+    public Vector3f add(Vector3f v){
         x += v.x;
         y += v.y;
         z += v.z;
+        return this;
     }
 
-    public void sub(Vector3f res, Vector3f v){
+    public Vector3f sub(Vector3f v, Vector3f res){
         res.x = x - v.x;
         res.y = y - v.y;
         res.z = z - v.z;
+        return res;
     }
 
-    public void sub(Vector3f v){
+    public Vector3f sub(Vector3f v){
         x -= v.x;
         y -= v.y;
         z -= v.z;
-    }
-
-    public float angleXY(){
-        if (x == 0){
-            if (y > 0) {
-                return 90;
-            }
-            else if (y < 0){
-                return 270;
-            }
-            return 0;
-        }
-        else {
-            float a = (float)Math.atan(y/x);
-            a = (float)Math.toDegrees(a);
-            if (x < 0) {
-                a += 180f;
-            }
-            return a;
-        }
+        return this;
     }
 
     public float distance(Vector3f v){
@@ -187,10 +182,11 @@ public class Vector3f implements Serializable {
         return res.length();
     }
 
-    public void copy(Vector3f v){
+    public Vector3f copy(Vector3f v){
         x = v.x;
         y = v.y;
         z = v.z;
+        return this;
     }
 
     public Vector3f clone(){
